e9493337a06b78705b432683260785a5f40112b8
Nikita <69157423+nktshklv-dev@users.noreply.github.com>
2022-06-12 15:09:44 +0300

    Created the basic UI

--- Whitehouse Petitions/ViewController.swift
+++ Whitehouse Petitions/ViewController.swift
@@ -23,7 +23,7 @@ class ViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         cell.textLabel?.text = "Title goes here ."
-        cell.detailTextLabel?.text = "Subtitle goes here"
+        cell.detailTextLabel?.text = "Subtitle goes here ."
         return cell
     }
     
